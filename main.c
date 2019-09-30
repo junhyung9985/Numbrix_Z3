29a522187d42a6fb3f616d2d8c477a5e66da7409
YoungChae <21300039@handong.edu>
2019-10-01 01:02:02 +0900

    mistake

--- main.c
+++ main.c
@@ -158,7 +158,7 @@ int main ()
     
     for (z = 0 ; z < (row+2)*(col+2) ; z++) {
         fscanf(fin,"%s %c%d %s %s", b, &position, &where, b, b);
-        fscanf(fin, " %d%c", &value, &close )
+        fscanf(fin, " %d%c", &value, &close );
         printf("%c%d %d%c\n", position, where, value, close);
         i = where/10;
         j =  where%10;
