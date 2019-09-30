461229a23f08924fd7cdcb8843d239a5c7767a06
YoungChae <21300039@handong.edu>
2019-10-01 02:52:02 +0900

    ???

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ int main (int argc, char **argv)
     
     for (int i =0; i<row+2; i++){
         for (int j=0; j<col+2; j++){
-            a[i][j] = 0;
+            a[i][j] = -1;
             //printf("%d", a[i][j]);
         }
         //printf("\n");
